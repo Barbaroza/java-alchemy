abd1ba8058b38fc6baed3cd1b750e6ce99ff821d
Barbaroza <169154615@qq.com>
2019-12-01 16:55:28 +0800

    practise makes better

---
+++ coding/src/main/java/com.pmb/code/backtracking/Permute.java
@@ -0,0 +1,48 @@
+package com.pmb.code.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 全排列
+ * 给定一个没有重复数字的序列，返回其所有可能的全排列。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,2,3]
+ * 输出:
+ * [
+ * [1,2,3],
+ * [1,3,2],
+ * [2,1,3],
+ * [2,3,1],
+ * [3,1,2],
+ * [3,2,1]
+ * ]
+ *
+ * @author lvrui
+ */
+public class Permute {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> temp, int[] nums) {
+        if (temp.size() == nums.length) {
+            list.add(new ArrayList<>(temp));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (temp.contains(nums[i]))
+                    continue;
+                temp.add(nums[i]);
+                backtrack(list, temp, nums);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+
+
+}
--- coding/src/main/java/com.pmb/code/backtracking/Subsets.java
+++ coding/src/main/java/com.pmb/code/backtracking/Subsets.java
@@ -1,9 +1,6 @@
 package com.pmb.code.backtracking;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 子集
@@ -29,28 +26,100 @@ import java.util.List;
  * @author lvrui
  */
 public class Subsets {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = null;
-        if (nums != null && nums.length > 0) {
-            Arrays.sort(nums);
-            res = new ArrayList<>();
-            for (int i = 0; i < nums.length; i++) {
-                for (int n = i; n < nums.length; n++) {
-                    List<Integer> temp = new ArrayList<>();
-                    for (int start = i; start <= n; start++) {
-                        temp.add(nums[start]);
-                    }
-                    res.add(temp);
-                }
-            }
-            res.add(new ArrayList<>());
-        }
-        return res;
 
+    public List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for (int n : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(n);
+                result.add(subset);
+            }
+        }
+        return result;
     }
+
+    /**
+     * DFS，前序遍历
+     */
+    public static void preOrder(int[] nums, int i, ArrayList<Integer> subset, List<List<Integer>> res) {
+        if (i >= nums.length) return;
+        // 到了新的状态，记录新的路径，要重新拷贝一份
+        subset = new ArrayList<Integer>(subset);
+
+        // 这里
+        res.add(subset);
+        preOrder(nums, i + 1, subset, res);
+        subset.add(nums[i]);
+        preOrder(nums, i + 1, subset, res);
+    }
+
+    /**
+     * DFS，中序遍历
+     */
+    public static void inOrder(int[] nums, int i, ArrayList<Integer> subset, List<List<Integer>> res) {
+        if (i >= nums.length) return;
+        subset = new ArrayList<Integer>(subset);
+
+        inOrder(nums, i + 1, subset, res);
+        subset.add(nums[i]);
+        // 这里
+        res.add(subset);
+        inOrder(nums, i + 1, subset, res);
+    }
+
+    /**
+     * DFS，后序遍历
+     */
+    public static void postOrder(int[] nums, int i, ArrayList<Integer> subset, List<List<Integer>> res) {
+        if (i >= nums.length) return;
+        subset = new ArrayList<Integer>(subset);
+
+        postOrder(nums, i + 1, subset, res);
+        subset.add(nums[i]);
+        postOrder(nums, i + 1, subset, res);
+        // 这里
+        res.add(subset);
+    }
+
+    public static void newPreOrder(int[] nums, int i, LinkedList<Integer> stack, List<List<Integer>> res) {
+        if (i >= nums.length) return;
+        stack.push(nums[i]);
+        // 这里
+        res.add(new ArrayList<Integer>(stack));
+        newPreOrder(nums, i + 1, stack, res);
+        stack.pop();
+        newPreOrder(nums, i + 1, stack, res);
+    }
+
+
+    public List<List<Integer>> subsets3(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums != null) {
+            Stack<Integer> path = new Stack<>();
+            int length = nums.length;
+            dfs(result, path, length, 0, nums);
+        }
+        return result;
+    }
+
+    private void dfs(List<List<Integer>> result, Stack<Integer> path, int length, int i, int[] nums) {
+        result.add(new ArrayList<>(path));
+
+        for (int index = i; index < length; index++) {
+            path.add(nums[index]);
+            dfs(result, path, length, index + 1, nums);
+            path.pop();
+        }
+    }
+
 
     public static void main(String[] args) {
         Subsets sb = new Subsets();
-        sb.subsets(new int[]{1, 2, 3});
+        List<List<Integer>> lists = sb.subsets3(new int[]{1, 2, 3});
+        lists.forEach(System.out::println);
+
     }
 }

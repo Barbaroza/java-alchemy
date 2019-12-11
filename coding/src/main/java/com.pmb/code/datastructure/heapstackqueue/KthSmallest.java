bb722e29733103166d1ed4c3c7527f9b4a68cab1
Barbaroza <169154615@qq.com>
2019-12-11 20:14:56 +0800

    practise makes better

--- coding/src/main/java/com.pmb/code/datastructure/heapstackqueue/KthSmallest.java
+++ coding/src/main/java/com.pmb/code/datastructure/heapstackqueue/KthSmallest.java
@@ -49,6 +49,64 @@ public class KthSmallest {
         return 0;
     }
 
+    public int kthSmallest2(int[][] matrix, int k) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int left = matrix[0][0];
+        int right = matrix[row - 1][col - 1];
+        while (left < right) {
+            // 每次循环都保证第K小的数在start~end之间，当start==end，第k小的数就是start
+            int mid = (left + right) / 2;
+            // 找二维矩阵中<=mid的元素总个数
+            int count = findNotBiggerThanMid(matrix, mid, row, col);
+            if (count < k) {
+                // 第k小的数在右半部分，且不包含mid
+                left = mid + 1;
+            } else {
+                // 第k小的数在左半部分，可能包含mid
+                right = mid;
+            }
+        }
+        return right;
+    }
+
+    private int findNotBiggerThanMid(int[][] matrix, int mid, int row, int col) {
+        // 以列为单位找，找到每一列最后一个<=mid的数即知道每一列有多少个数<=mid
+        int i = row - 1;
+        int j = 0;
+        int count = 0;
+        while (i >= 0 && j < col) {
+            if (matrix[i][j] <= mid) {
+                // 第j列有i+1个元素<=mid
+                count += i + 1;
+                j++;
+            } else {
+                // 第j列目前的数大于mid，需要继续在当前列往上找
+                i--;
+            }
+        }
+        return count;
+    }
+
+    public int kthSmallest3(int[][] matrix, int k) {
+        int n = matrix.length;
+        int lo = matrix[0][0], hi = matrix[n - 1][n - 1];
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int count = 0, j = matrix[0].length - 1;
+            for (int i = 0; i < matrix.length; i++) {
+                while (j >= 0 && matrix[i][j] > mid) j--;
+                count += (j + 1);
+            }
+            if (count < k)
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        return lo;
+    }
+
+
     /**
      * [[1,5,9],[10,11,13],[12,13,15]]
      *
@@ -61,7 +119,7 @@ public class KthSmallest {
         a[2] = new int[]{11, 14, 14};
 
         KthSmallest abc = new KthSmallest();
-        int sadasd = abc.kthSmallest(a, 3);
+        int sadasd = abc.kthSmallest2(a, 3);
         sadasd = 2;
     }
 }

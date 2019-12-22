d744dc635198a2368cae0cc0ba8cbae01c45b308
Barbaroza <169154615@qq.com>
2019-12-23 05:57:23 +0800

    practise makes better

---
+++ coding/src/main/java/com.pmb/code/datastructure/stack/SimplifyPath.java
@@ -0,0 +1,33 @@
+package com.pmb.code.datastructure.stack;
+
+import java.io.File;
+import java.util.Stack;
+
+/**
+ * @author lvrui
+ */
+public class SimplifyPath {
+    String FILE_SEPERATE = File.separator;
+
+    public String simplifyPath(String path) {
+        String[] s = path.split("/");
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < s.length; i++) {
+            if (!stack.isEmpty() && s[i].equals(".."))
+                stack.pop();
+            else if (!s[i].equals("") && !s[i].equals(".") && !s[i].equals(".."))
+                stack.push(s[i]);
+        }
+        if (stack.isEmpty())
+            return "/";
+
+        StringBuffer res = new StringBuffer();
+        for (int i = 0; i < stack.size(); i++) {
+            res.append("/" + stack.get(i));
+        }
+        return res.toString();
+    }
+
+
+}
